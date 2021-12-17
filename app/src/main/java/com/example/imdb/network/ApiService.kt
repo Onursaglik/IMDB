623df647e78afc1e6811ec75db117909f1a04c8b
Onursaglik <68904434+Onursaglik@users.noreply.github.com>
2021-12-17 20:15:57 +0300

    Update ApiService.kt

--- app/src/main/java/com/example/imdb/network/ApiService.kt
+++ app/src/main/java/com/example/imdb/network/ApiService.kt
@@ -9,19 +9,19 @@ import retrofit2.http.Query
 interface ApiService {
 
     @GET("discover/movie?")
-    suspend fun getPopularSeries(@Query("api_key") api_key: String = "fe5337337edbe1635db7285cec6be023",
+    suspend fun getPopularSeries(@Query("api_key") api_key: String = "***",
                                  @Query("sort_by") sort_by: String = "popularity.desc",
                                  @Query("page") page: Int): ApiResponse
 
     @GET("search/movie?")
-    suspend fun getSearchSeries(@Query("api_key") api_key: String = "fe5337337edbe1635db7285cec6be023",
+    suspend fun getSearchSeries(@Query("api_key") api_key: String = "***",
                                 @Query("query") query: String,
                                 @Query("page") page: Int): ApiResponse
 
     @GET("movie/{id}?")
     suspend fun getMovieDetail(
             @Path("id") id: Int?,
-            @Query("api_key") api_key: String = "fe5337337edbe1635db7285cec6be023",
+            @Query("api_key") api_key: String = "***",
     ): Movie
 
 }
